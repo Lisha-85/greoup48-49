aff38397eef977b2331084a03272ed9602ca78d8
linabarbe <linabarbe@web.de>
2022-12-08 12:15:47 +0100

    Lasson1

---
+++ src/main/java/Lasson1.java
@@ -0,0 +1,71 @@
+public class Lasson1 {
+    public static void main (String[] args){
+        //задача номер 1//
+     /*
+        int a= 54;
+        int b= 16;
+
+        System.out.println(a + b );
+
+        int c = 50;
+        int d = 3;
+
+        System.out.println(c/d );
+
+        int e = -5;
+        int f = 8 ;
+        int g = 6;
+
+        System.out.println (-e + f *6);
+
+        int n = 5;
+
+
+
+        System.out.println (n*n);
+
+        int z = 0;
+        int k = 1;
+        int i = 2;
+        int r = 3;
+        int s = 4;
+        int o = 5;
+        int p = 6;
+        int q = 7;
+        int u = 8;
+        int v = 9;
+        int m = 45;
+
+        System.out.println(m/10);
+
+
+
+        int a = 1000;
+        int b = 500;
+
+        int sum = a+b;
+        double discount = sum * 0.1;
+        double result
+
+
+         System.out.println (a+b)/10;
+
+      */
+
+   double x1= 10.5;
+   double x2= 2.5;
+
+   double resDouble = x1/x2;
+
+   int resInt = (int)  (x1/x2);
+
+   System.out.print(resDouble + " "+ resInt);
+   System.out.print("");
+
+
+        
+
+
+
+    }
+}
